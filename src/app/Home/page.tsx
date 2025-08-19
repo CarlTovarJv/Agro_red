84afb983ed59ca2b81e437e46e6587925f1e0e02
MINEDUCYT <mateoantoniomenjivarmelara@gmail.com>
2025-08-19 07:59:07 -0600

    HOme

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,9 @@
+function lore() {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste numquam aperiam quod quidem. Iure praesentium eaque ad, similique quae aliquam porro ea sapiente ut molestias? Ab autem velit soluta sit.</h1>
+    </div>
+  )
+}
+
+export default lore
