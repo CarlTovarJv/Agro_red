99fb3ab1f8ea3594a03977063b1355c32bf2e332
Aisack23 <00016023@uca.edu.sv>
2025-09-03 23:07:20 -0600

    navbars finished

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,38 +1,31 @@
 "use client";
 
 import Image from "next/image";
-import { FaUser, FaSearch } from "react-icons/fa";
+import { FaUser, FaBars } from "react-icons/fa";
 
-export default function Navbar() {
+export default function NavbarLogin() {
   return (
     <header
       className="w-full bg-white border-b border-gray-200 shadow-sm relative"
       style={{ height: 48 }}
     >
       <div className="max-w-8xl mx-auto ml-6 h-full flex items-center justify-between px-4 relative">
+        
+        {/* Left: Hamburger + Logo */}
         <div className="flex items-center gap-3">
-          <Image src="/menu.svg" className="cursor-pointer" alt="Menu" width={28} height={28} />
-          <Image className="cursor-pointer"
-            src="/Agrored-1-removebg-preview.png"
+          {/* Hamburger menu */}
+          <button className="text-gray-700 text-xl">
+            <FaBars />
+          </button>
+
+          {/* Logo */}
+          <Image
+            className="cursor-pointer"
+            src="/Logo_C.svg"
             alt="AgroRed"
             width={95}
             height={37}
           />
-        </div>
-
-        <div
-          className="absolute left-1/2 transform -translate-x-1/2"
-          style={{ width: 400}}
-        >
-          <div className="relative">
-            <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-            <input
-              type="text"
-              aria-label="Search"
-              placeholder="Search"
-              className="w-full h-9 pl-10 pr-3 bg-gray-100 border border-gray-200 text-sm text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded-xl"
-            />
-          </div>
         </div>
 
         {/* Right: Sign in */}
