3a926b8a8baf9ce1994421eb03d2da530698bc02
Mateo PC <mateoantoniomenjivarmelara@gmail.com>
2025-11-12 08:37:56 -0600

    cambio route

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,15 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prismaClient";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  const id = Number(params.id);
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
+  const id = Number(context.params.id);
 
   const user = await prisma.users.findUnique({
     where: { user_id: id },
   });
 
-  if (!user) return NextResponse.json({ error: "Usuario no encontrado" }, { status: 404 });
+  if (!user) {
+    return NextResponse.json({ error: "Usuario no encontrado" }, { status: 404 });
+  }
 
   return NextResponse.json(user);
-  
 }
